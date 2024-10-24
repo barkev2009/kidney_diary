918d220f5ccbc06b72acde942b9ceb0c9db57c6a
Greg Kovshov <barkev2009@bk.ru>
2024-10-25 00:27:31 +0700

    prod client

--- start_client_prod.sh
+++ start_client_prod.sh
@@ -2,6 +2,7 @@ cd client
 echo "Installing client deps..."
 npm i
 npm run build
+nohup serve -s build -l 3004 &
 # rm -r /var/www/build
 # mv build /var/www
-nohup node index.js &
+# nohup node index.js &
