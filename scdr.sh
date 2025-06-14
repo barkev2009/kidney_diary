2a915506ec468c90b1e99956677586a6ed9df3dd
Greg Kovshov <barkev2009@bk.ru>
2025-06-14 14:10:50 +0700

    fix

--- scdr.sh
+++
@@ -1,3 +0,0 @@
-cd client
-npm run build
-scp -r build/* root@62.217.182.73:/var/www/kidney_diary
--- ssdr.sh
+++
@@ -1,7 +0,0 @@
-cd server
-rm -rf server.tar.gz
-tar --exclude='node_modules' -zcvf server.tar.gz .
-ssh root@62.217.182.73 "rm -rf kidney-diary-server/*"
-scp server.tar.gz root@62.217.182.73:~/kidney-diary-server/server.tar.gz
-ssh root@62.217.182.73 "pm2 stop kidney-diary-back; tar -xvzf kidney-diary-server/server.tar.gz -C kidney-diary-server; ls kidney-diary-server; cd kidney-diary-server; npm i; pm2 start pm2.json"
-cd ..
